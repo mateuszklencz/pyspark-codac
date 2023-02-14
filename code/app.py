609a5aec96f86ecbc7cf2b0808cc66a3474118bb
KLENCZ <mateusz.klencz@capgemini.com>
2023-02-14 19:17:11 +0100

    after some refactor - pytest does not work

--- code/app.py
+++ code/app.py
@@ -1,50 +1,39 @@
-#app.py dataset_one.csv dataset_two.csv -c Netherlands 'United Kingdom'
-import argparse
-import pyspark 
-from pyspark.sql import SparkSession
+#python3 app.py ../data/dataset_one.csv ../data/dataset_two.csv -c France
 import os
 import shutil
 import logging
+import argparse
+import pyspark 
+from pyspark.sql import SparkSession
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
+from typing import Tuple
+from datachanger import DataChanger
+
+class OutputBuilder():
+
+	'''
+	self.logger: logging.LogRecord
+	self.dataset1: str
+	self.dataset2: str
+	self.countries: list
+	self.spark: pyspark.sql.SparkSession
+	self.df1: pyspark.sql.DataFrame
+	self.df2: pyspark.sql.DataFrame
+	self.df_merged: pyspark.sql.DataFrame
+	'''
+	def __init__(self):
+		self.logger: logging.LogRecord
+		self.dataset1: str
+		self.dataset2: str
+		self.countries: list
+		self.spark: pyspark.sql.SparkSession
+		self.df1: pyspark.sql.DataFrame
+		self.df2: pyspark.sql.DataFrame
+		self.df_merged: pyspark.sql.DataFrame
 
 
-class DataChanger:
-
-	@staticmethod
-	def renameColumns(df: pyspark.sql.dataframe.DataFrame, columns: dict) -> pyspark.sql.dataframe.DataFrame:
-		'''
-		Generic function for renaming columns in provided PySpark DataFrame. It returns DataFrame with changed names. 
-		Original df remains without any changes (unless replaced by the result of this method). 
-
-		:df: pyspark dataframe in which columns name should be renamed
-		:columns: python dictionary in which key is the old name and value is the new name
-		:output: pyspark dataframe which is copy of input dataframe but with changed columns name
-		'''
-		for key in columns:
-			df=df.withColumnRenamed(key,columns[key])
-
-		return df
-
-	@staticmethod
-	def filterCountry(df: pyspark.sql.dataframe.DataFrame, col_name: str, filter: list) -> pyspark.sql.dataframe.DataFrame:
-		'''
-		Generic function for filtering columns in provided PySpark DataFrame. It returns filtered original dataframe. 
-		Original df remains without any changes (unless replaced by the result of this method). 
-
-		:df: pyspark dataframe in which data should be filtered using *filter* parameter
-		:col_name: name of column to which filter should be applied
-		:filter: list of ** strings ** for which equality filtering will take place (string == value in column)
-		:output: pyspark dataframe which is copy of input dataframe but limited only to rows with a data from filter list values
-		'''
-
-		df=df[df[col_name].isin(filter)]
-
-		return df
-
-
-	@staticmethod
-	def getLogger() -> logging.LogRecord:
+	def getLogger(self):
 		'''
 		Method creates a properly configured instance of logging from python standard library
 		with a * RotatingFileHandler * and level set to * INFO *
@@ -57,80 +46,119 @@ class DataChanger:
 		handler = RotatingFileHandler(filename="logs.log", maxBytes=1024, backupCount=5)
 		logger.addHandler(handler)
 
-		return logger
+		self.logger = logger
+
+		self.logReq('log started')
+
+		return self
+
+
+	def parseArgs(self):
+
+		self.logReq('parsing parameters')
+
+		#parse arguments of filepaths of used datasets and countries 
+		parser = argparse.ArgumentParser(description='pyspark codac assignment')
+		parser.add_argument('path1', nargs=1, default='../data/dataset_one.csv',
+			help='system path to first dataset', type=str)
+		parser.add_argument('path2', nargs=1, default='../data/dataset_two.csv',
+			help='system path to second dataset', type=str)
+		parser.add_argument('-c', nargs='*', default=["United Kingdom", "Netherlands"],
+			help='countries for which extract data separeted by space', required=False)
+
+		parameters = vars(parser.parse_args())
+
+		filepath1 = parameters['path1'][0]
+		filepath2 = parameters['path2'][0]
+		country_list = parameters['c']
+
+		self.dataset1 = filepath1
+		self.dataset2 = filepath2
+		self.countries = country_list
+
+		print(self.dataset1)
+		print(self.dataset2)
+		print(self.countries)
+
+		return self
+
+
+	def settingSparkSession(self):
+		self.logReq('starting spark')
+
+		self.spark=SparkSession.builder.appName('mk').getOrCreate()
+
+		return self
+
+
+	def loadingDataFrames(self):
+		self.logReq('loading dataframes')
+
+		self.df1=self.spark.read.csv(self.dataset1, header=True, inferSchema=True)
+		self.df2=self.spark.read.csv(self.dataset2, header=True, inferSchema=True)
+
+		return self
+
+	def droppingUnusedColumns(self, list_of_columns_for_df1: list, list_of_columns_for_df2: list):
+		self.logReq('deleting unused columns')
+
+		for c in list_of_columns_for_df1:
+			self.df1=self.df1.drop(c)
+
+		for c in list_of_columns_for_df2:
+			self.df2=self.df2.drop(c)
+
+		return self
+
+	def renameColumns(self, dict_for_df1: dict, dict_for_df2: dict):
+		self.logReq('renaming columns')
+
+		self.df1=DataChanger.renameColumns(self.df1, dict_for_df1)
+		self.df2=DataChanger.renameColumns(self.df2, dict_for_df2)
+
+		return self
+
+	def mergeDataFrames(self, column_id: str):
+		self.logReq('merging dataframes')
+
+		self.mergedDf=self.df1.join(self.df2, column_id, 'left')
+
+		return self
+
+	def filterByCountry(self):
+		self.logReq('filtering by country')
+
+		self.df1=DataChanger.filterCountry(self.df1, 'country', self.countries)
+
+		return self
+
+
+	def saveMergedOutput(self, directory='../client_data'):
+		self.logReq('saving output')
+
+
+		if(os.path.exists(directory)):
+			shutil.rmtree(directory) #deleting existing
+
+		self.mergedDf.write.csv(directory)
+
+	def logReq(self, msg: str):
+		if self.logger != None:
+			self.logger.info(str(datetime.now()) + ": " + msg)
 
 
 # ------------------------------------execution part
 
 if __name__ == "__main__":
 
-	#creating logging instance
-	logger = DataChanger.getLogger()
-	logger.info(str(datetime.now()) + ': starting app')
-	
-	#parse arguments of filepaths of used datasets and countries 
-	parser = argparse.ArgumentParser(description='pyspark codac assignment')
-	parser.add_argument('path1', nargs=1)
-	parser.add_argument('path2', nargs=1)
-	parser.add_argument('-c', nargs='*')
-
-	parameters = vars(parser.parse_args())
-
-	logger.info(str(datetime.now()) + ': parsing parameters')
-
-	filepath1 = parameters['path1'][0]
-	filepath2 = parameters['path2'][0]
-	country_list = parameters['c']
-
-	logger.info(str(datetime.now()) + ': running pyspark')
-
-	#1 starting PySpark
-	spark=SparkSession.builder.appName('mk').getOrCreate()
-
-	logger.info(str(datetime.now()) + ': loading data')
-
-	#2 loading dataframes
-	df1=spark.read.csv(filepath1, header=True, inferSchema=True)
-	df2=spark.read.csv(filepath2, header=True, inferSchema=True)
-
-	logger.info(str(datetime.now()) + ': filtering data')
-
-	#3 filtering through countries
-	df1=DataChanger.filterCountry(df1, 'country', country_list)
-
-	#4 dropping unused column 
-	df1=df1.drop('first_name','last_name','country')
-	df2=df2.drop('cc_n')
-
-	logger.info(str(datetime.now()) + ': renaming column')
-
-	#5 renaming columns 
-	df1_dict = {'id' : 'client_identifier'}
-
-	df2_dict= {
+	output = OutputBuilder().getLogger().parseArgs()\
+		.settingSparkSession().loadingDataFrames()\
+		.filterByCountry().droppingUnusedColumns(['first_name','last_name','country'],['cc_n'])\
+		.renameColumns({'id' : 'client_identifier'}, {
 		'id' : 'client_identifier',
 		'btc_a' : 'bitcoin_address',
 		'cc_t' : 'credit_card_type'
-	}
+		}).mergeDataFrames('client_identifier')
 
-	df1=DataChanger.renameColumns(df1, df1_dict)
-	df2=DataChanger.renameColumns(df2, df2_dict)
 
-	logger.info(str(datetime.now()) + ': merging data')
-
-	#6 merging 
-	df = df1.join(df2, 'client_identifier', 'left')
-
-	logger.info(str(datetime.now()) + ': saving output')
-
-	#7 saving output
-
-	#first check if specifing directory exists and if not create it 
-	directory='../client_data'
-
-	if(os.path.exists(directory)):
-		shutil.rmtree(directory) #deleting existing
-
-	df.write.csv(directory)
-
-	logger.info(str(datetime.now()) + ': Success! All tasks are completed')
+	output.saveMergedOutput()
---
+++ test/test_datachanger.py
@@ -0,0 +1,78 @@
+import sys
+import pytest
+import pyspark 
+from pyspark.sql import SparkSession
+from chispa import assert_df_equality
+sys.path.append('../')
+from code.datachanger import DataChanger
+
+
+class TestGroup:
+
+	@pytest.fixture
+	def spark_configuration(self) -> SparkSession:
+		'''
+		Fixture function which provide spark session for others tests
+		'''
+		spark = SparkSession.builder \
+      .master("local") \
+      .appName("chispa") \
+      .getOrCreate()
+
+		return spark
+
+	def test_renameColumns(self, spark_configuration):
+		'''
+		function to test static method * renameColumns * from DataChanger class. 
+
+		It uses sample data to check wheter direct pyspark methods to **rename columns** have the same output as
+		method from custom class. 
+
+		It uses chispa testing and *assert_df_equality()* 
+		'''
+		data = [
+			('sample11', 'sample12', 'sample13'),
+			('sample21', 'sample22', 'sample23'),
+			('sample31', 'sample32', 'sample33')
+		]
+
+		df = (spark_configuration.createDataFrame(data, ['col1', 'col2', 'col3']))
+
+		expected_df = df.withColumnRenamed('col1', 'column1')
+		expected_df = expected_df.withColumnRenamed('col3', 'column3')
+
+		actual_df = DataChanger.renameColumns(df, {
+			'col1' : 'column1',
+			'col3' : 'column3'
+			})
+
+		assert_df_equality(actual_df, expected_df)
+
+
+	def test_filterCountry(self, spark_configuration):
+
+		'''
+		function to test static method *filterCountry()* from DataChanger class. 
+
+		It uses sample data to check wheter direct pyspark methods to **filter strings (isin)** have the same output as
+		method from custom class. 
+
+		It uses chispa testing and *assert_df_equality()* 
+		'''
+
+		data = [
+			('Germany', 'Berlin', 'Europe'),
+			('Poland', 'Warsaw', 'Europe'),
+			('Germany', 'Berlin', 'Europe'),
+			('Poland', 'Warsaw', 'Europe'),
+			('Austria', 'Vienna', 'Europe'),
+			('Poland', 'Warsaw', 'Europe')
+		]
+
+		df = (spark_configuration.createDataFrame(data, ['country', 'city', 'continent']))
+
+		expected_df = df.filter((df['city'].like('Berlin')) | (df['city'].like('Vienna'))) 
+
+		actual_df = DataChanger.filterCountry(df, 'city', ['Berlin', 'Vienna'])
+
+		assert_df_equality(actual_df, expected_df)
